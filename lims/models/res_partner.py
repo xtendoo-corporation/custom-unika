37478a56d5bbccf5bb2bb5f63110a204ef51a70f
dani <dani.xtendoo@gmail.com>
2024-07-22 09:54:08 +0200

    Añadido botón para ver los analisis en la ficha del cliente

--- lims/models/res_partner.py
+++ lims/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2021 - Manuel Calero <https://xtendoo.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class ResPartner(models.Model):
@@ -21,3 +21,42 @@ class ResPartner(models.Model):
         string="Enviar email de recpción",
         default=True,
     )
+
+    analysis_count = fields.Integer(
+        "Number of Analysis Generated",
+        compute="_compute_analysis_count",
+    )
+
+
+    def _compute_analysis_count(self):
+        for partner in self:
+            partner.analysis_count = len(self._get_analysis())
+
+    def _get_analysis(self):
+        return self.env["lims.analysis.line"].search(
+            [("customer_id", "=", self.id)]
+        )
+
+    def action_view_analysis(self):
+        self.ensure_one()
+        analysis_line_ids = self._get_analysis().ids
+        action = {
+            "res_model": "lims.analysis.line",
+            "type": "ir.actions.act_window",
+        }
+        if len(analysis_line_ids) == 1:
+            action.update(
+                {
+                    "view_mode": "form",
+                    "res_id": analysis_line_ids[0],
+                }
+            )
+        else:
+            action.update(
+                {
+                    "name": _("Analisis del cliente  %s", self.name),
+                    "domain": [("id", "in", analysis_line_ids)],
+                    "view_mode": "tree,form",
+                }
+            )
+        return action
