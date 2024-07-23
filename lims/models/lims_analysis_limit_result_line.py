cd7915a127a28cc3b3efff711c2b5421db932857
dani <dani.xtendoo@gmail.com>
2024-07-23 09:31:50 +0200

    sync

--- lims/models/lims_analysis_limit_result_line.py
+++ lims/models/lims_analysis_limit_result_line.py
@@ -197,9 +197,6 @@ class LimsAnalysisParameterLimitResultLine(models.Model):
             if self.type == "ISPRESENT":
                 limit = "Presente" if self.is_present else "No Presente"
             elif self.type == "ISCORRECT":
-                print("*" *100)
-                print(self.parent_id.parameter_ids.limit_value_char)
-                print("*" *100)
                 limit = self.parent_id.parameter_ids.limit_value_char if self.parent_id.parameter_ids.limit_value_char else ""
             elif self.type == "LIMIT":
                 limit = self._get_limit_value_char()
