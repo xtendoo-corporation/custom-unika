9bdd4e0dcf752580433abea109772b02a1242bca
dani <dani.xtendoo@gmail.com>
2024-09-13 09:07:38 +0200

    [ADD] unika_administration

---
+++ unika_administration/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Not Show journal",
+    "summary": """Not Show journal""",
+    "version": "15.0.1.0.0",
+    "description": """Not Show journal""",
+    "author": "Daniel Dominguez",
+    "company": "Xtendoo",
+    "website": "https://github.com/OCA/edi",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        'security/security_group.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
